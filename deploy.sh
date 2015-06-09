90559205550f76f8464d8a91a1901f7acdb8ffc8
Qijin Zhou <qijin.zhou@outlook.com>
2015-06-09 00:31:37 -0700

    Integrate with Azure.

--- deploy.sh
+++ deploy.sh
@@ -117,6 +117,9 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
+# 4. Create Hubot file with a coffee extension
+cp "$DEPLOYMENT_TARGET/node_modules/hubot/bin/hubot" "$DEPLOYMENT_TARGET/node_modules/hubot/bin/hubot.coffee"
+
 ##################################################################################################################################
 
 # Post deployment stub
